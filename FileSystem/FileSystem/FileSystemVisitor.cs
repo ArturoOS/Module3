7da20ce676d348c7a12e71fb3ec62dcf181e1cea
ArturoOS <Arturo_Osorio1@epam.com>
2023-10-23 16:48:40 -0600

    Initial commit

---
+++ FileSystem/FileSystem/FileSystemVisitor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileSystem
+{
+	public class FileSystemVisitor
+	{
+		public delegate string Filter(string pathToFilter);
+		public event EventHandler OnStart;
+		public event EventHandler OnFinish;
+		public event EventHandler OnFileFound;
+		public event EventHandler OnDirectoryFound;
+		public event EventHandler OnFilteredFileFound;
+		public event EventHandler OnFilteredDirectoryFound;
+		private bool FilterSearch = false;
+		private Filter filterDelegate;
+
+		public FileSystemVisitor() {}
+		public FileSystemVisitor(Filter filterFunction) 
+		{
+			FilterSearch = true;
+			filterDelegate = filterFunction;
+		}
+
+		public IEnumerable<string> SearchForFilesAndFolders(string path) 
+		{
+			OnStart?.Invoke(this, EventArgs.Empty);
+
+			string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+			foreach (var file in files)
+			{
+				OnFileFound?.Invoke(this, EventArgs.Empty);
+				if (!FilterSearch)
+				{
+					yield return file + "\n";
+				}
+				else 
+				{
+					if (!string.IsNullOrEmpty(filterDelegate(file)))
+					{
+						yield return file + "\n";
+					}
+					else 
+					{
+						OnFilteredFileFound?.Invoke(this, EventArgs.Empty);
+						yield return "";
+					}
+				}
+			}
+
+			string[] directories = Directory.GetDirectories(path, "*.*", SearchOption.AllDirectories);
+			foreach (var directory in directories)
+			{
+				OnDirectoryFound?.Invoke(this, EventArgs.Empty);
+				if (!FilterSearch)
+				{
+					yield return directory + "\n";
+				}
+				else
+				{
+					if (!string.IsNullOrEmpty(filterDelegate(directory)))
+					{
+						yield return directory + "\n";
+					}
+					else 
+					{
+						OnFilteredDirectoryFound?.Invoke(this, EventArgs.Empty);
+						yield return "";
+					}
+				}
+			}
+
+			OnFinish?.Invoke(this, EventArgs.Empty);
+		}
+	}
+}
---
+++ FileSystem/FileSystem/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileSystem
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public event EventHandler<bool> OnCancel;
+		public bool IsCancel;
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			FolderBrowserDialog openFileDlg = new FolderBrowserDialog();
+			openFileDlg.ShowDialog();
+
+			if (!string.IsNullOrEmpty(openFileDlg.SelectedPath))
+			{
+				DirectoryBox.Text = openFileDlg.SelectedPath;
+				
+				FileSystemVisitor fileSystemVisitor;
+				if (!string.IsNullOrWhiteSpace(FilterBox.Text))
+				{
+					string nameToExclude = FilterBox.Text;
+					fileSystemVisitor = new FileSystemVisitor(file => file.Contains(nameToExclude) ? string.Empty : file);
+				}
+				else
+					fileSystemVisitor = new FileSystemVisitor();
+
+				fileSystemVisitor.OnStart += FileSystemVisitor_OnStart;
+				fileSystemVisitor.OnFinish += FileSystemVisitor_OnFinish;
+				fileSystemVisitor.OnFileFound += FileSystemVisitor_OnFileFound;
+				fileSystemVisitor.OnDirectoryFound += FileSystemVisitor_OnDirectoryFound;
+				fileSystemVisitor.OnFilteredFileFound += FileSystemVisitor_OnFilteredFileFound;
+				fileSystemVisitor.OnFilteredDirectoryFound += FileSystemVisitor_OnFilteredDirectoryFound;
+				OnCancel += MainWindow_OnCancel;
+				
+				foreach (var item in fileSystemVisitor.SearchForFilesAndFolders(openFileDlg.SelectedPath))
+				{
+					ResultBox.Text += item;
+					await Task.Delay(500); // Just to show events happening
+					if (IsCancel) break;
+				}
+			}
+		}
+
+		private void FileSystemVisitor_OnFilteredDirectoryFound(object sender, EventArgs e)
+		{
+			FoundLabel.Content = "Filtered Directory Found";
+		}
+
+		private void FileSystemVisitor_OnFilteredFileFound(object sender, EventArgs e)
+		{
+			FoundLabel.Content = "Filtered File Found";
+		}
+
+		private void FileSystemVisitor_OnDirectoryFound(object sender, EventArgs e)
+		{
+			FoundLabel.Content = "Directory Found";
+		}
+
+		private void FileSystemVisitor_OnFileFound(object sender, EventArgs e)
+		{
+			FoundLabel.Content = "File Found";
+		}
+
+		private void FileSystemVisitor_OnStart(object sender, EventArgs e)
+		{
+			SearchLabel.Content = "Searching...";
+			FilterBtn.IsEnabled = false;
+			CancelBtn.IsEnabled = true;
+			ResultBox.Text = "";
+			IsCancel = false;
+		}
+		private void FileSystemVisitor_OnFinish(object sender, EventArgs e)
+		{
+			SearchLabel.Content = "Done.";
+			FilterBtn.IsEnabled = true;
+			CancelBtn.IsEnabled = false;
+		}
+		private void MainWindow_OnCancel(object sender, bool e)
+		{
+			IsCancel = e;
+			SearchLabel.Content = "Canceled.";
+			CancelBtn.IsEnabled = false;
+			FilterBtn.IsEnabled = true;
+		}
+
+		private void CancelBtn_Click(object sender, RoutedEventArgs e)
+		{
+			OnCancel?.Invoke(this, true);
+		}
+
+		private void FilterBtn_Click(object sender, RoutedEventArgs e)
+		{
+			string filterString = FilterBox.Text;
+			if (!string.IsNullOrWhiteSpace(filterString))
+			{
+				List<string> resultList = new List<string>(ResultBox.Text.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries));
+				ResultBox.Text = "";
+				foreach (var result in resultList)
+				{
+
+					if (!result.Contains(filterString))
+					{
+						ResultBox.Text += result + "\n";
+					}
+
+				}
+			}
+		}
+	}
+}
